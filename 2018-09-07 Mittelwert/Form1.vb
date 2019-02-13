d1e330b3b7ce97e1ef4a8954737206a34b1015fb
Benedikt Michael Schneider <bs762448@fh-muenster.de>
2019-02-13 11:05:19 +0100

    Projektdateien hinzufügen.

---
+++ 2018-09-07 Mittelwert/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub cmdMittel_Click(sender As Object, e As EventArgs) Handles cmdMittel.Click
+        Dim mM As Single, mZahl As Single, mS As Single, mA As Integer
+        mZahl = InputBox("mZahl (0=Ende)?")
+        Do While mZahl <> "0"
+            mA = mA + 1
+            mS = mS + (mZahl)
+            MsgBox(mA, mZahl, mS)
+
+            mZahl = InputBox("mZahl (0=Ende)?")
+        Loop
+        If mA <> 0 Then
+            mM = mS / mA
+            MsgBox(mM)
+        End If
+
+    End Sub
+End Class
